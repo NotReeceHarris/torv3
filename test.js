708ad133d78707117ebf12e07c7f3d844f299a69
Reece Harris <reece@codegalaxy.co.uk>
2023-04-25 15:07:44 +0100

    modified:   test.js

--- test.js
+++ test.js
@@ -3,4 +3,4 @@ const onionv3 = require('./build/index');
 const keys = onionv3.generateKeys();
 const v3 = onionv3.generateOnionV3(keys.privateKey);
 
-console.log(Buffer.compare(keys.publicKey, v3.publicKey) === 0 && v3.verified);
+process.exit(Buffer.compare(keys.publicKey, v3.publicKey) === 0 && v3.verified ? 1 : 0);
